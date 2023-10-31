02f3230f2146bf9368af977d6ba6b8230ddeb052
Hancheol Cho <chcbaram@gmail.com>
2023-10-31 20:58:37 +0900

    I2C 예제 추가

--- firmware/cube/Core/Src/main.c
+++ firmware/cube/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "dma.h"
+#include "i2c.h"
 #include "rtc.h"
 #include "usart.h"
 #include "gpio.h"
@@ -97,6 +98,7 @@ int main(void)
   MX_DMA_Init();
   MX_USART1_UART_Init();
   MX_RTC_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
